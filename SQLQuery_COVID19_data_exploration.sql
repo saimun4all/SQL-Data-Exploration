252ca09666089f8647cf32579ce75ef5d7cbed46
Saimun Hassan <saimunhas@gmail.com>
2023-08-13 15:54:48 -0400

    Exploring total deaths based on the total number of cases in different regions

--- SQLQuery_COVID19_data_exploration.sql
+++ SQLQuery_COVID19_data_exploration.sql
@@ -1,2 +1,21 @@
 SELECT * FROM dbo.CovidDeaths
 order BY 3,4
+
+--SELECT * FROM dbo.CovidVaccinations
+--ORDER BY 3,4
+
+SELECT location, date,total_cases,new_cases, total_deaths, population
+FROM dbo.CovidDeaths
+order BY 1,2
+
+-- Looking into Total Cases vs Total Deaths
+
+SELECT 
+	Location,
+	date,
+	total_cases,
+	total_deaths,
+	(total_deaths/total_cases)*100 AS Death_Percentage
+FROM
+	dbo.CovidDeaths
+ORDER BY 1,2
