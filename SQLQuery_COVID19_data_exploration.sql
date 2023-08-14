b5655b77b6dd83114525dfb940dfb50ef60db476
Saimun Hassan <saimunhas@gmail.com>
2023-08-13 21:57:38 -0400

    Shows what percentage of population got Covid in the US

--- SQLQuery_COVID19_data_exploration.sql
+++ SQLQuery_COVID19_data_exploration.sql
@@ -1,14 +1,14 @@
-SELECT * FROM dbo.CovidDeaths
+SELECT * FROM PortfolioProject.dbo.CovidDeaths
 order BY 3,4
 
 --SELECT * FROM dbo.CovidVaccinations
 --ORDER BY 3,4
 
 SELECT location, date,total_cases,new_cases, total_deaths, population
-FROM dbo.CovidDeaths
+FROM PortfolioProject.dbo.CovidDeaths
 order BY 1,2
 
--- Looking into Total Cases vs Total Deaths
+-- Looking into Total Cases vs Total Deaths in different countries
 
 SELECT 
 	Location,
@@ -17,5 +17,21 @@ SELECT
 	total_deaths,
 	(total_deaths/total_cases)*100 AS Death_Percentage
 FROM
-	dbo.CovidDeaths
+	PortfolioProject.dbo.CovidDeaths
+WHERE location LIKE '%states%'
+ORDER BY 1,2
+
+
+-- Looking into Total Cases vs Population
+-- Shows what percentage of population got Covid
+
+SELECT 
+	Location,
+	date,
+	total_cases,
+	population,
+	(total_cases/population)*100 AS DeathPercentage
+FROM
+	PortfolioProject.dbo.CovidDeaths
+WHERE location LIKE '%states%'
 ORDER BY 1,2
